71267555a56750de650e58abd73e9782a0c90054
renatofagalde <renato@likwi.com.br>
2024-01-14 20:50:53 -0300

    [commit 🍀] first deploy test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,5 @@ import {Router} from "express";
 export const router= Router()
 
 router.get('/',(request,response)=>{
-    response.send('API com express')
+    response.send('API com express via deploy')
 })
