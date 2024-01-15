cc3b4c9b4c0f7b50579b08066883950675114cb1
renatofagalde <renato@likwi.com.br>
2024-01-14 21:28:07 -0300

    [commit 🍀] deploy com o Dan

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,5 @@ import {Router} from "express";
 export const router= Router()
 
 router.get('/',(request,response)=>{
-    response.send('API com express via deploy')
+    response.send('API com express via deploy com o Dan')
 })
